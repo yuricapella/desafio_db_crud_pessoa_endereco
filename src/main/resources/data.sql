02e22fd867065105b8c3c6a718b348c06f7adba9
Yuri Capella <96320879+yuricapella@users.noreply.github.com>
2025-04-16 11:23:03 -0300

    feat: adiciona pessoas no banco de dados para testes

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO pessoa (nome, cpf, data_nascimento, data_criacao)
+VALUES
+    ('João Silva', '123.456.789-00', '1995-04-16', NOW()),
+    ('Maria Oliveira', '234.567.890-11', '1998-08-22', NOW()),
+    ('Pedro Santos', '345.678.901-22', '1989-12-10', NOW()),
+    ('Ana Rodrigues', '456.789.012-33', '1996-05-05', NOW()),
+    ('Lucas Pereira', '567.890.123-44', '1985-07-30', NOW());
