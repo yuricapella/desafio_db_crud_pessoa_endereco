e7a432233766f73967181ce15ac0b855cf834eae
Yuri Capella <96320879+yuricapella@users.noreply.github.com>
2025-04-15 23:28:09 -0300

    versão inicial do projeto gerada pelo spring initializr

---
+++ src/main/java/br/com/db/desafio_crud_pessoa_endereco/DesafioCrudPessoaEnderecoApplication.java
@@ -0,0 +1,13 @@
+package br.com.db.desafio_crud_pessoa_endereco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioCrudPessoaEnderecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioCrudPessoaEnderecoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/db/desafio_crud_pessoa_endereco/DesafioCrudPessoaEnderecoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.db.desafio_crud_pessoa_endereco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCrudPessoaEnderecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
