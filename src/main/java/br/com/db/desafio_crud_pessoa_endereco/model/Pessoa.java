0c29a55fd0183e57d44390bb7d8fc5320500cde2
Yuri Capella <96320879+yuricapella@users.noreply.github.com>
2025-04-16 08:58:31 -0300

    feat: adiciona pessoa e endereço em model

---
+++ src/main/java/br/com/db/desafio_crud_pessoa_endereco/model/Pessoa.java
@@ -0,0 +1,63 @@
+package br.com.db.desafio_crud_pessoa_endereco.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "PESSOA")
+public class Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String cpf;
+    private LocalDate dataNascimento;
+    private LocalDateTime dataCriacao;
+    private LocalDateTime dataAtualizacao;
+
+    public Pessoa() {
+        this.dataCriacao = LocalDateTime.now();
+    }
+
+    public Pessoa(Long id, String nome, String cpf, LocalDate dataNascimento) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+}
